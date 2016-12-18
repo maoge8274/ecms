f11494ae575a0838b906ff00c9c0d975f9505827
leohe <hemaojun@foxmail.com>
2016-12-18 21:05:28 +0800

    edit

--- controllers/admin/base.go
+++ controllers/admin/base.go
@@ -4,10 +4,10 @@ import "github.com/astaxie/beego"
 
 type baseController struct {
 	beego.Controller
-	userid int64
-	username string
-	nickname string
+	userid         int64
+	username       string
+	nickname       string
 	controllerName string
-	actionName string
-	admindir string
+	actionName     string
+	admindir       string
 }
